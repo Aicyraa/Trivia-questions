ae78887479ad69fc718e50a88008728e8db08d34
Jee <182109647+Aicyraa@users.noreply.github.com>
2025-08-12 18:38:33 +0800

    Fixed sgit cipt.js
    Made the fetching of trivia quesiton work

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+const getBtn = document.getElementById("getQst");
 
-let diff = 'medium'
-let catergory = ''
-let amount = ''
+async function getTrivia(amount, category, difficulty) {
+  const result = await fetch(
+    `https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}&type=multiple`
+  );
+  const data = await result.json();
+  return data;
+}
 
-// fetch(`https://opentdb.com/api.php?amount=10&category=13&difficulty=${diff}&type=multiple`)
-// .then(res => res.json())
-// .then(res => console.log(res));
+getBtn.addEventListener("click", (e) => {
+  let amount = document.querySelector(".trivia__amount");
+  let category = document.querySelector(".trivia__category");
+  let difficulty = document.querySelector(".trivia__difficulty");
+  
+  getTrivia(amount.value, category.value, difficulty.value);
+  //let data = getTrivia(amount.value, category.value, difficulty.value);
+});
